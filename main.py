f2a2594fcb3ca7a5df98a8e6fad3faa34492a0cb
Vitor Cavalcante <v9cavalcante@gmail.com>
2022-08-08 15:40:18 -0300

    refactor: changed font and fixed coordinates

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
+import string
 from PIL import Image, ImageFont, ImageDraw
 from pathlib import Path
 
 
-coord_pessoa = (147,384)
-coord_horas = (148,656)
-coord_setor = (527,654)
-coord_data_inicio = (610,741)
-coord_data_saida = (827,741)
+coord_pessoa = (143, 391)
+coord_horas = (143, 656)
+coord_setor = (525, 656)
+coord_data_inicio = (600, 744)
+coord_data_saida = (815, 744)
 
 nome_pessoa = input('Insira o nome do membro: ')
 #fazer o calculo automático da carga horária
@@ -20,20 +21,26 @@ setor = input("Insira o cargo do membro: ")
 
 script_location = Path(__file__).absolute().parent
 file_location = script_location/'certificado.png'
+print(script_location)
 
 file_save_location = script_location/'output'/'{}.png'.format(nome_pessoa)
 
+cwd = os.getcwd()
+# print(os.path.join(cwd,  "arial-nova-bold.ttf"))
 imagem = Image.open(file_location)
-caminho_fonte = r"C:\Windows\Fonts\ARIALN.TTF"
-font = ImageFont.truetype(caminho_fonte,40)
+caminho_fonte = os.path.join(cwd,  "arial-nova.ttf")
+caminho_fonte_bold = os.path.join(cwd,  "arial-nova-bold.ttf")
+font = ImageFont.truetype(caminho_fonte, 36)
+font_bold = ImageFont.truetype(caminho_fonte_bold, 36)
 rgb_preto = (0,0,0)
 desenho = ImageDraw.Draw(imagem)
 
-desenho.text(coord_pessoa,nome_pessoa,font=font,fill=rgb_preto,stroke_width=1)
-desenho.text(coord_horas,carga_horaria,font=font,fill=rgb_preto,stroke_width=1)
-desenho.text(coord_setor,setor,font=font,fill=rgb_preto)
-desenho.text(coord_data_inicio,data_inicio,font=font,fill=rgb_preto,stroke_width=1)
-desenho.text(coord_data_saida,data_saida,font=font,fill=rgb_preto,stroke_width=1)
+desenho.text(coord_pessoa, "{name},".format(name = nome_pessoa),font=font_bold, fill=rgb_preto)
+desenho.text(coord_horas, carga_horaria, font=font_bold, fill=rgb_preto)
+desenho.text(coord_setor, setor, font=font, fill=rgb_preto)
+# desenho.text(coord_data_inicio, "{0}  -  {1}".format(data_inicio, data_saida), font=font_bold, fill=rgb_preto)
+desenho.text(coord_data_inicio, data_inicio, font=font_bold, fill=rgb_preto)
+desenho.text(coord_data_saida, data_saida, font=font_bold, fill=rgb_preto)
 
 imagem.save(file_save_location)
 imagem.show()
